0d15e5f31d618c8015cc7e71267b26f0e6fe1e5f
carmonterr <jcmonterroza7@misena.edu.co>
2024-06-10 12:43:01 -0500

    palabra obsoleta

--- back/config/database.js
+++ back/config/database.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const connectDatabase = () =>{
     mongoose.connect(process.env.DB_URI,
        { 
-       useUnifiedTopology:true
+    
     }).then(con => {
         console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
     })
